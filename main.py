a49fe801f350522af660a7a7aeb0a07b63883489
adam.promo@gmail.com <adam.promo@gmail.com>
2021-11-01 18:09:18 +0200

    init project

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 import yfinance as yf
 import argparse
 import yaml
+from munch import DefaultMunch
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 
-def init_parser():
+def args_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-tk", "--ticker", help="Ticker symbol name")
+    parser.add_argument("-s", "--symbol", help="Symbol name")
+    parser.add_argument("-t", "--time_period", help="Time period")
 
     return parser.parse_args()
 
 
-def fetch_ticker(args):
-    return yf.Ticker(args.ticker)
+def load_config():
+    config_dict = yaml.safe_load(open("config.yaml"))
+    return DefaultMunch.fromDict(config_dict)
+
+
+def fetch_ticker(symbol):
+    return yf.Ticker(symbol)
 
 
 def main():
-    args = init_parser()
-    ticker = fetch_ticker(args)
+    args = args_parser()
+    config = load_config()
+    ticker = fetch_ticker(config.symbol)
+    symbol_df = ticker.history(period=config.time_period)
 
+    print(symbol_df)
 
-def load_to_db():
-    pass
+    client = InfluxDBClient(url=config.db.url, token=config.db.token)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
 
+    write_api.write(org=config.db.org, bucket=config.db.bucket, record=symbol_df, data_frame_measurement_name=config.symbol)
 
-
-
+main()
