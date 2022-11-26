a200439f939d48c76aabd3e11999e549820502e8
Adam Yodinsky <adam.yodinsky@autodesk.com>
2022-11-26 20:47:31 +0200

    moving to dotenv

---
+++ data_downloader/data_downloader.py
@@ -0,0 +1,78 @@
+from timescale import TmDB
+import yahoo
+import utils
+import datetime
+from dateutil.relativedelta import relativedelta
+import logging
+import os
+import click
+
+
+@click.group()
+@click.option('-p', '--data-period', type=int, default=None, help='How many years back to get data from. (<number>y).')
+@click.option('-i', '--data-interval', type=str, default=None, help='Intervals of data points, <number>h for hours, <number>d for days.')
+@click.pass_context
+def cli(ctx, data_period: int = None, data_interval: str = None):
+    # Load env variables from .env file
+    utils.load_env("files/.env")
+
+    # Set logger configuration
+    logging.basicConfig(
+        format="%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+        datefmt="%Y-%m-%d:%H:%M:%S",
+        level=logging.INFO,
+    )
+
+    ctx.obj["data_period"] = data_period or os.environ.get("DATA_PERIOD")
+    ctx.obj["data_interval"] = data_interval or os.environ.get("DATA_INTERVAL")
+    ctx.obj["current_date"] = datetime.date.today()
+    ctx.obj["db"] = TmDB()
+    
+
+@click.command(
+    help=f"Download historical price data for a list of stocks"
+)
+@click.option('-n', '--number-of-tickers', type=int, default=None, help='Number of tickers to iterate over.')
+def download_data(ctx, number_of_tickers: int = None):
+    number_of_tickers = number_of_tickers or os.environ.get("NUMBER_OF_TICKERS")
+    tickers = ctx.obj["db"].get_tickers_list()
+
+    # Download stocks data and save into DB
+    for index, ticker in enumerate(tickers):
+        # a limit for a number of stocks we want to get data on from the list, this probably be used only in dev.
+        if number_of_tickers and index >= number_of_tickers:
+            break
+
+        download_ticker_data(ctx, ticker[0])
+
+    logging.info("Stocks data updated successfully.")
+
+
+@click.command(
+    help=f"Download a specific stock historical price data"
+)
+@click.option('-t', '--ticker', type=str, help="Stock ticker")
+def download_ticker_data(ctx, ticker: str):
+    start = utils.get_starting_date(ctx.obj["db"], ticker, ctx.obj["data_period"], ctx.obj["current_date"])
+
+    # Download stock data and save into DB
+    if start != ctx.obj["current_date"]:  # prevent an API call when there is no need
+        ticker_data = yahoo.download_prices(
+            ticker=ticker,
+            start=start,
+            end=ctx.obj["current_date"],
+            interval=ctx.obj["data_interval"],
+        )
+        logging.info(f"Uploading {ticker} data to DB.")
+        ctx.obj["db"].upsert_data(
+            df=ticker_data, table=os.environ.get("DB_STOCK_PRICE_TABLE")
+        )
+    else:
+        logging.info(f"{ticker} Is up to date, no action needed.")
+
+    logging.info("Data updated successfully.")
+
+
+cli.add_command(download_data)
+
+cli()
--- data_downloader/utils.py
+++ data_downloader/utils.py
@@ -5,3 +5,36 @@ from pathlib import Path
 def load_env(path: str):
     dotenv_path = Path(path)
     load_dotenv(dotenv_path=dotenv_path)
+
+
+def get_starting_date(db: TmDB, ticker: str, data_period: str, current_date):
+    """Get the first date from where we want the data series we downloading will begin from.
+    If there is no data at all, it will download the data by the DATA_PERIOD env variable.
+    If there is data, but there is a new data that should be downloaded, or the data_period is going further back from our oldest data_point, the missing data will be downloaded and added to the existing data.
+    """
+
+    start = None
+    first_data_point_date = db.get_first(
+        table=os.environ.get("DB_STOCK_PRICE_TABLE"), ticker=ticker
+    )
+    last_data_point_date = db.get_last(
+        table=os.environ.get("DB_STOCK_PRICE_TABLE"), ticker=ticker
+    )
+    period_date = (
+        current_date
+        - relativedelta(years=data_period)
+        + relativedelta(weeks=1)
+    )
+
+    # if period go further in the past compared to the oldest record, use period
+    # else, use the last data-point date as a starting point which is the most fresh data point.
+    if last_data_point_date is not None:
+        if period_date < first_data_point_date[1]:
+            start = period_date
+        else:
+            start = last_data_point_date[1]
+    else:
+        start = period_date
+
+    return start
+
